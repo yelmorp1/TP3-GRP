9ae36a17b1465c5d7c87f7c559c19e6ff649be85
Roger Paucar <paucar.roger@gmail.com>
2017-09-07 23:03:44 -0500

    modificación de vistas

--- models/controlcosto.js
+++ models/controlcosto.js
@@ -1,14 +1,14 @@
-app.factory("controlCostoFtry") {
+app.factory("controlCostoFtry", function ($http, $location, $rootScope) {
     return {
         get: function (id) {
             return $http({
-                url: $rootScope.baseUrl + '/articulo/' + id,
+                url: $rootScope.baseUrl + '/solicitudretiro/' + id,
                 method: 'GET'
             });
         },
         getAll: function () {
             return $http({
-                url: $rootScope.baseUrl + '/articulo',
+                url: $rootScope.baseUrl + '/solicitudretiro',
                 method: 'GET'
             });
         }
